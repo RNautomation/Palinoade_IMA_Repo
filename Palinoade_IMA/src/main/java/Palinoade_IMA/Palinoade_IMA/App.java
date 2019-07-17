073f0b48a8b3be6e8d5c13e99cb3a578bc58e42e
RUCHI <RUCHI@192.168.1.4>
2019-07-17 18:01:07 +0530

    created on 17-07-2019

---
+++ Palinoade_IMA/src/main/java/Palinoade_IMA/Palinoade_IMA/App.java
@@ -0,0 +1,13 @@
+package Palinoade_IMA.Palinoade_IMA;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
